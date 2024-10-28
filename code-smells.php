5b9f8dc70ea6f58169f80fb2604047d64ca6b14f
Gary Kovar <gkovar@gmail.com>
2024-10-28 13:46:33 +0000

    start state

--- code-smells.php
+++ code-smells.php
@@ -1,52 +1,5 @@
 <?php
 
-function tax_rate() {
-  return 0.1;
-}
-
-class Checkout
-{
-  public function calculateTotal($cartItems)
-  {
-    $total = 0;
-    foreach ($cartItems as $item) {
-      $priceWithTax = $item['price'] + ($item['price'] * tax_rate()); // Tax applied here
-      $total += $priceWithTax * $item['quantity'];
-    }
-    return $total;
-  }
-}
-
-class Invoice
-{
-  public function generateInvoice($order)
-  {
-    $subtotal = 0;
-    foreach ($order->items as $item) {
-      $subtotal += $item->price * $item->quantity;
-    }
-    $tax = $subtotal * tax_rate();
-    return $subtotal + $tax;
-  }
-}
-
-class Report
-{
-  public function generateSalesReport($orders)
-  {
-    $totalSales = 0;
-    foreach ($orders as $order) {
-      $total = 0;
-      foreach ($order->items as $item) {
-        $priceWithTax = $item->price + ($item->price * tax_rate());
-        $total += $priceWithTax * $item->quantity;
-      }
-      $totalSales += $total;
-    }
-    return $totalSales;
-  }
-}
-
 class Author
 {
   public $name;
